ff04ab0608399c81ab3227add51e235246f22898
Smily-zack <155127760+Smily-zack@users.noreply.github.com>
2024-10-31 07:08:53 -0400

    add more css and do some changes

--- node.js
+++ node.js
@@ -5,4 +5,4 @@ document.write("<table border='1'>");
     for(i=1;i<=10;i++){
         document.write("<tr><th>",user_input,"</th><th> X </th><th>",i,"</th><th> = </th><th>",user_input*i,"</th></tr>");
     }
-    document.write("<table>");
+    document.write("</table>");
