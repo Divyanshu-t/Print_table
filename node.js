da7153cabdf3c3694913ff05949b7a1b97d84b61
Smily-zack <155127760+Smily-zack@users.noreply.github.com>
2024-10-31 06:33:54 -0400

    i=user_input replaced into i=1

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ alert("Enter Any Number For Print His Table ") ;
 let user_input = parseInt(prompt("Enter Number")) ;
 
 document.write("<table border='1'>");
-    for(i=user_input;i<=10;i++){
+    for(i=1;i<=10;i++){
         document.write("<tr><th>",user_input,"</th><th> X </th><th>",i,"</th><th> = </th><th>",user_input*i,"</th></tr>");
     }
     document.write("<table>");
